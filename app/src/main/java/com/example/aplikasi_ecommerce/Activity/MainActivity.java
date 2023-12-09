21b0b34a47ff957dff9ff2f1c3a1cf4bd17e6507
Abiyyah <abiyyahr@gmail.com>
2023-12-09 15:13:46 +0700

    "feat: Part 2 finish"

---
+++ app/src/main/java/com/example/aplikasi_ecommerce/Activity/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.aplikasi_ecommerce.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.aplikasi_ecommerce.Adapter.PopularAdapter;
+import com.example.aplikasi_ecommerce.Domain.PopularDomain;
+import com.example.aplikasi_ecommerce.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapterPopular;
+    private  RecyclerView recyclerViewPopular;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        ArrayList<PopularDomain> items=new ArrayList<>();
+        items.add(new PopularDomain("T-Shirt Black","Lorem Ipsum Dolor sit met","pic1",15,4,500));
+        items.add(new PopularDomain("Smart Watch","Lorem Ipsum Dolor sit met","pic2",10,4.5,450));
+        items.add(new PopularDomain("Iphone 14","Lorem Ipsum Dolor sit met","pic3",15,4.3,800));
+        items.add(new PopularDomain("VisionX pro LED TV","Lorem Ipsum Dolor sit met","pic4",18,4.0,1500));
+
+        recyclerViewPopular=findViewById(R.id.view1);
+        recyclerViewPopular.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        adapterPopular = new PopularAdapter(items);
+        recyclerViewPopular.setAdapter(adapterPopular);
+    }
+}
---
+++ app/src/main/java/com/example/aplikasi_ecommerce/Adapter/PopularAdapter.java
@@ -0,0 +1,69 @@
+package com.example.aplikasi_ecommerce.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.GranularRoundedCorners;
+import com.example.aplikasi_ecommerce.Domain.PopularDomain;
+import com.example.aplikasi_ecommerce.R;
+
+import java.util.ArrayList;
+
+public class PopularAdapter extends RecyclerView.Adapter<PopularAdapter.Viewholder> {
+
+    ArrayList<PopularDomain> items;
+
+
+
+    Context context;
+    public PopularAdapter(ArrayList<PopularDomain> items) {
+        this.items = items;
+    }
+    @NonNull
+    @Override
+    public PopularAdapter.Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context=parent.getContext();
+        View inflate= LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_pup_list,parent,false);
+        return new Viewholder(inflate);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PopularAdapter.Viewholder holder, int position) {
+holder.titleTxt.setText(items.get(position).getTitle());
+holder.feeTxt.setText("$"+items.get(position).getPrice());
+holder.scoreTxt.setText(""+items.get(position).getScore());
+
+int drawableResourceId=holder.itemView.getResources().getIdentifier(items.get(position).getPicUrl(),
+        "drawable", holder.itemView.getContext().getPackageName());
+Glide.with(holder.itemView.getContext())
+        .load(drawableResourceId)
+        .transform(new GranularRoundedCorners(30,30,0,0))
+        .into(holder.pic);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class Viewholder extends RecyclerView.ViewHolder {
+        TextView titleTxt, feeTxt, scoreTxt;
+        ImageView pic;
+
+        public Viewholder(@NonNull View itemView) {
+            super(itemView);
+            titleTxt=itemView.findViewById(R.id.titleTxt);
+            feeTxt=itemView.findViewById(R.id.feeTxt);
+            scoreTxt=itemView.findViewById(R.id.scoreTxt);
+            pic=itemView.findViewById(R.id.p);
+        }
+    }
+}
