f052b1f69a3aff02929a80adcc361f87b0f58eb9
Abiyyah <abiyyahr@gmail.com>
2023-12-09 20:34:34 +0700

    "feat: Part 4 finish"

---
+++ app/src/main/java/com/example/aplikasi_ecommerce/Activity/CartActivity.java
@@ -0,0 +1,16 @@
+package com.example.aplikasi_ecommerce.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.aplikasi_ecommerce.R;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
